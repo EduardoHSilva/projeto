464e22b0778001b94ce2f2df5e5c027cd1a1ed23
EduardoHSilva <henriqueeof@gmail.com>
2026-02-08 17:53:48 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatarr.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  /*
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+    */
+}
